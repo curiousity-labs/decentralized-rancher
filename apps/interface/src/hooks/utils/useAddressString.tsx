82d1101d29d01e19db90680688b5b24f8136de49
David Colon <38386583+Da-Colon@users.noreply.github.com>
2022-11-23 00:05:46 -0500

    updates address ens look to work properly

--- apps/interface/src/hooks/utils/useAddressString.tsx
+++ apps/interface/src/hooks/utils/useAddressString.tsx
@@ -1,24 +1,35 @@
-import { useMemo } from "react"
-import { useEnsName } from "wagmi"
+import { useCallback, useEffect, useState } from "react"
+import { useProvider } from "wagmi"
 import { EthAddress } from "../../types/common"
 import { useStringMethods } from "./useStringMethods"
 
+
 export const useAddressString = (address: EthAddress | undefined) => {
-  const { data, isLoading } = useEnsName({
-    address,
-  })
+  const [displayName, setDisplayName] = useState('');
+  const provider = useProvider()
+
+  const lookupENS = useCallback(async (address: EthAddress) => {
+    return await provider.lookupAddress(address).catch()
+  }, [provider])
+  
   const { addressSubString } = useStringMethods();
 
-  const displayName = useMemo(() => {
-    if (!address) {
-      return ''
+  useEffect(() => {
+    if(!address) {
+      setDisplayName('');
+      return;
     }
-    if (!data && !isLoading) {
-      return addressSubString(address)
-    }
-    return data;
-  }, [address, data, isLoading, addressSubString])
 
+    (async () => {
+      const ensname = await lookupENS(address);
+      if(ensname) {
+        setDisplayName(ensname)
+        return
+      }
+      setDisplayName(addressSubString(address))
+    })()
+
+  }, [address, addressSubString, lookupENS])
 
   return { displayName }
 }
